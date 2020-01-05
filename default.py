64365f452ac8fdf48b0056d0e8d03b040c7418fc
MRLB <156-53-35-651@gmx.de>
2020-01-05 12:32:02 +0100

    added schedule

--- default.py
+++ default.py
@@ -46,6 +46,12 @@ def availableInTerritoryCheck(i,id):
                 availableInTerritory = True
                 break
             j = j + 1
+    elif id == 2:
+        while j < len(espplayerSchedule['included'][i]['attributes']['playableTerritories']['territories']):
+            if espplayerSchedule['included'][i]['attributes']['playableTerritories']['territories'][j] == territory:
+                availableInTerritory = True
+                break
+            j = j + 1
     return availableInTerritory
 
 def zeitformatierung(time):
@@ -71,6 +77,14 @@ def bildurlherausfinden(i, id):
             if espplayerArchiveAuswahl['included'][j]['id'] == espplayerArchiveAuswahl['included'][i]['relationships']['images']['data'][0][
                 'id']:
                 bildurl = espplayerArchiveAuswahl['included'][j]['attributes']['src']
+                break
+            j = j + 1
+    elif id == 2:
+        while j < len(espplayerSchedule['included']):
+            if espplayerSchedule['included'][j]['id'] == \
+                    espplayerSchedule['included'][i]['relationships']['images']['data'][0][
+                        'id']:
+                bildurl = espplayerSchedule['included'][j]['attributes']['src']
                 break
             j = j + 1
     return bildurl
@@ -103,7 +117,7 @@ user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:71.0) Gecko/20100101 
 header = {
     'Host': 'eu3-prod-direct.eurosportplayer.com',
     'User-Agent': user_agent,
-    'Cookie': '[Cookie hier eingeben]',
+    'Cookie': '[Cookie hier eintragen]',
 }
 
 #territory = 'de'
@@ -113,6 +127,7 @@ urlStream1 = 'https://eu3-prod-direct.eurosportplayer.com/playback/v2/videoPlayb
 urlStream2 = '?usePreAuth=true'
 urlArchiveAuswahl = 'https://eu3-prod-direct.eurosportplayer.com/cms/routes/on-demand?include=default'
 urlStart = 'https://eu3-prod-direct.eurosportplayer.com/cms/routes'
+urlSchedule = 'https://eu3-prod-direct.eurosportplayer.com/cms/routes/schedule?include=default'
 
 if mode is None:
     #Territory herausfinden
@@ -141,7 +156,6 @@ if mode is None:
                     #5 min vor Streamstart anzeigen
                     if (datetime.datetime.utcnow() - datetime_start > datetime.timedelta(seconds=-300)):
                         datetime_start_local = datetime_start + datetime.timedelta(seconds=int(umrechnungZeitzoneUnterschiedSekunden()))
-
                         sender = espplayerMain['included'][i]['attributes']['path'].split('/')
 
                         #endzeit
@@ -166,11 +180,137 @@ if mode is None:
             i1 = i1 + 1
         i = i + 1
     createOrdner('Archive', 'Archive')
+
+    'Schedule:'
+    foldername = '--------------------------------------'
+    li = xbmcgui.ListItem(foldername, iconImage='DefaultFolder.png')
+    xbmcplugin.addDirectoryItem(handle=addon_handle, url='', listitem=li)
+
+    createOrdner('Schedule', 'Schedule Eurosport 1')
+    createOrdner('Schedule', 'Schedule Eurosport 2')
+
+    foldername = '--------------------------------------'
+    li = xbmcgui.ListItem(foldername, iconImage='DefaultFolder.png')
+    xbmcplugin.addDirectoryItem(handle=addon_handle, url='', listitem=li)
+
+    foldername ='Nächste Liveevents:'
+    li = xbmcgui.ListItem(foldername, iconImage='DefaultFolder.png')
+    xbmcplugin.addDirectoryItem(handle=addon_handle, url='', listitem=li)
+
+    espplayerSchedule = requests.get(url=urlSchedule, headers=header)
+    espplayerSchedule = espplayerSchedule.json()
+    i = 0
+    i1 = 0
+    availableInTerritory = False
+    j = 0
+    while i < len(espplayerSchedule['included']):
+        # print(str(i))
+        try:
+            if espplayerSchedule['included'][i]['attributes']['broadcastType'] == 'LIVE':
+                if availableInTerritoryCheck(i, 2):
+                    datetime_start = zeitformatierung(
+                        espplayerSchedule['included'][i]['attributes']['availabilityWindows'][0]['playableStart'])
+                    # 5 min vor Streamstart anzeigen
+                    if (datetime.datetime.utcnow() - datetime_start < datetime.timedelta(seconds=-299)):
+                        datetime_start_local = datetime_start + datetime.timedelta(
+                            seconds=int(umrechnungZeitzoneUnterschiedSekunden()))
+
+                        sender = espplayerSchedule['included'][i]['attributes']['path'].split('/')
+
+                        # endzeit
+                        datetime_ende = zeitformatierung(espplayerSchedule['included'][i]['attributes']['scheduleEnd'])
+                        datetime_ende_local = datetime_ende + datetime.timedelta(
+                            seconds=umrechnungZeitzoneUnterschiedSekunden())
+
+                        foldername = str(
+                            str(datetime_start_local)[8:10]+'.'+str(datetime_start_local)[5:7]+'.'+str(datetime_start_local)[0:4]+' '+str(datetime_start_local)[11:16] + ' - ' + str(datetime_ende_local)[11:16] + ' Uhr: ' +
+                            sender[0] + ': ' + espplayerSchedule['included'][i]['attributes']['name'] + ' (' +
+                            espplayerSchedule['included'][i]['attributes']['materialType'] + ')')
+
+                        url = build_url({'mode': 'playStream', 'foldername': foldername,
+                                         'streamID': espplayerSchedule['included'][i]['id']})
+                        li = xbmcgui.ListItem(foldername, iconImage=bildurlherausfinden(i, 2))
+                        li.setProperty('IsPlayable', 'true')
+                        li.setInfo('video', {'plot': str(
+                            espplayerSchedule['included'][i]['attributes']['scheduleStart'] + ' - ' +
+                            espplayerSchedule['included'][i]['attributes']['secondaryTitle'])})
+                        xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+        except KeyError:
+            i1 = i1 + 1
+        i = i + 1
+
+
+
     xbmcplugin.endOfDirectory(addon_handle)
 
     #except:
     #    xbmcgui.Dialog().ok(_addon_name, str(meData))
     #    xbmcplugin.setResolvedUrl(_addon_handler, False, xbmcgui.ListItem())
+
+elif mode[0] == 'Schedule':
+    if args['foldername'][0] == 'Schedule Eurosport 1':
+        suchsender = 'eurosport-1'
+        foldername = 'Schedule Eurosport 1:'
+        li = xbmcgui.ListItem(foldername, iconImage='DefaultFolder.png')
+        xbmcplugin.addDirectoryItem(handle=addon_handle, url='', listitem=li)
+
+    elif args['foldername'][0] == 'Schedule Eurosport 2':
+        suchsender = 'eurosport-2'
+        foldername ='Schedule Eurosport 2:'
+        li = xbmcgui.ListItem(foldername, iconImage='DefaultFolder.png')
+        xbmcplugin.addDirectoryItem(handle=addon_handle, url='', listitem=li)
+    else:
+        suchsender = ''
+        foldername = 'Error'
+        li = xbmcgui.ListItem(foldername, iconImage='DefaultFolder.png')
+        xbmcplugin.addDirectoryItem(handle=addon_handle, url='', listitem=li)
+
+    espplayerSchedule = requests.get(url=urlSchedule, headers=header)
+    espplayerSchedule = espplayerSchedule.json()
+    i = 0
+    i1 = 0
+    availableInTerritory = False
+    j = 0
+    while i < len(espplayerSchedule['included']):
+        # print(str(i))
+        try:
+            sender = espplayerSchedule['included'][i]['attributes']['path'].split('/')
+            if sender[0][0:11] == suchsender:
+                if availableInTerritoryCheck(i, 2):
+                    datetime_start = zeitformatierung(
+                        espplayerSchedule['included'][i]['attributes']['availabilityWindows'][0]['playableStart'])
+                    # 5 min vor Streamstart anzeigen
+                    if (datetime.datetime.utcnow() - datetime_start < datetime.timedelta(seconds=-299)):
+                        datetime_start_local = datetime_start + datetime.timedelta(
+                            seconds=int(umrechnungZeitzoneUnterschiedSekunden()))
+
+                        sender = espplayerSchedule['included'][i]['attributes']['path'].split('/')
+
+                        # endzeit
+                        datetime_ende = zeitformatierung(espplayerSchedule['included'][i]['attributes']['scheduleEnd'])
+                        datetime_ende_local = datetime_ende + datetime.timedelta(
+                            seconds=umrechnungZeitzoneUnterschiedSekunden())
+
+                        foldername = str(
+                            str(datetime_start_local)[8:10]+'.'+str(datetime_start_local)[5:7]+'.'+str(datetime_start_local)[0:4]+' '+str(datetime_start_local)[11:16] + ' - ' + str(datetime_ende_local)[11:16] + ' Uhr: ' +
+                            espplayerSchedule['included'][i]['attributes']['name'] + ' ('+sender[0] + ')')
+
+                        url = build_url({'mode': 'playStream', 'foldername': foldername,
+                                         'streamID': espplayerSchedule['included'][i]['id']})
+                        li = xbmcgui.ListItem(foldername, iconImage=bildurlherausfinden(i, 2))
+                        li.setProperty('IsPlayable', 'true')
+                        li.setInfo('video', {'plot': str(
+                            espplayerSchedule['included'][i]['attributes']['scheduleStart'] + ' - ' +
+                            espplayerSchedule['included'][i]['attributes']['secondaryTitle'])})
+                        xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+        except KeyError:
+            i1 = i1 + 1
+        i = i + 1
+
+
+
+    xbmcplugin.endOfDirectory(addon_handle)
+
 
 elif mode[0] == 'Archive':
     archiveAuswahl = requests.get(url=urlArchiveAuswahl, headers=header)
